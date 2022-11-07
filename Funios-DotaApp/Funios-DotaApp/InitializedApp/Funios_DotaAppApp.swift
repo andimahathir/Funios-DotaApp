99cb43c8770f9d6aa9d7da2a8fcec7a87874c72b
isa nur fajar <ishafajar11@gmail.com>
2022-11-07 11:26:29 +0700

    initApp

---
+++ Funios-DotaApp/Funios-DotaApp/InitializedApp/Funios_DotaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funios_DotaAppApp.swift
+//  Funios-DotaApp
+//
+//  Created by isa nur fajar on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Funios_DotaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
