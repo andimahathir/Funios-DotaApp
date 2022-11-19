9c102d32abfc77b42aada72c6243612aab21e062
Caroline Chan <carolinechan304@gmail.com>
2022-11-19 21:01:40 +0700

    ADD: home view

---
+++ Funios-DotaApp/Funios-DotaApp/Presentation/HomeHero/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  Funios-DotaApp
+//
+//  Created by Caroline Chan on 19/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
