e4a42afb946ec2042dab92e5bb41662d955590aa
Caroline Chan <carolinechan304@gmail.com>
2022-11-20 14:06:07 +0700

    ADD: dota hero model

---
+++ Funios-DotaApp/Funios-DotaApp/Presentation/HomeHero/Model/DotaHeroModel.swift
@@ -0,0 +1,28 @@
+//
+//  DotaHeroModel.swift
+//  Funios-DotaApp
+//
+//  Created by Caroline Chan on 20/11/22.
+//
+
+import Foundation
+
+// MARK: - DotaModelElement
+struct DotaHeroElement: Codable {
+    let id: Int
+    let name, localizedName: String
+    let primaryAttr: String
+    let attackType: String
+    let roles: [String]
+    let legs: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case localizedName = "localized_name"
+        case primaryAttr = "primary_attr"
+        case attackType = "attack_type"
+        case roles, legs
+    }
+}
+
+typealias DotaHeroModel = [DotaHeroElement]
