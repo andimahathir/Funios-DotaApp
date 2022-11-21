5a6919436935ab9742884a64586742e2d2d8ce61
Caroline Chan <carolinechan304@gmail.com>
2022-11-21 20:05:34 +0700

    FIX: only wrap highest hierarchy view with navigation view

--- Funios-DotaApp/Funios-DotaApp/InitializedApp/ContentView.swift
+++ Funios-DotaApp/Funios-DotaApp/InitializedApp/ContentView.swift
@@ -9,27 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                }
-            
-            Text("List of hero")
-                .tabItem {
-                    Image(systemName: "square.grid.2x2")
-                }
-            
-            Text("Heart")
-                .tabItem {
-                    Image(systemName: "heart")
-                }
-            
-            Text("Settings")
-                .tabItem {
-                    Image(systemName: "gearshape")
-                }
-            
+        NavigationView {
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                    }
+                
+                Text("List of hero")
+                    .tabItem {
+                        Image(systemName: "square.grid.2x2")
+                    }
+                
+                Text("Heart")
+                    .tabItem {
+                        Image(systemName: "heart")
+                    }
+                
+                Text("Settings")
+                    .tabItem {
+                        Image(systemName: "gearshape")
+                    }
+            }
+            .navigationTitle("Dota Hero")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
