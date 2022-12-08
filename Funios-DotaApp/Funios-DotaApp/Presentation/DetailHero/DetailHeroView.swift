198ea56607c3c8b47c65ebe51a675a130e4dd990
adedwi1808 <adedwip1808@gmail.com>
2022-12-08 20:45:15 +0700

    REF: Extract herostatsview

--- Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/DetailHeroView.swift
+++ Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/DetailHeroView.swift
@@ -46,82 +46,8 @@ struct DetailHeroView: View {
                     Color("BaseColor")
                         .cornerRadius(20)
                     
-                    VStack(alignment: .leading) {
-                        Text("Stats")
-                        HStack(spacing: 20) {
-                            VStack(alignment: .leading) {
-                                Text("Attributes")
-                                HStack {
-                                    Image("Strength")
-                                    Text(detailHeroViewModel.getHeroStrength())
-                                }
-                                
-                                HStack {
-                                    Image("Agility")
-                                    Text(detailHeroViewModel.getHeroAgility())
-                                }
-                                
-                                HStack {
-                                    Image("Intelligence")
-                                    Text(detailHeroViewModel.getHeroIntelligence())
-                                }
-                            }
-                            Color(.gray)
-                                .frame(width: 1, height: 150)
-                            
-                            VStack(alignment: .leading) {
-                                Text("Ability")
-                                HStack {
-                                    Image("AttackSymbol")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                    Text(detailHeroViewModel.getHeroBaseAttack())
-                                }
-                                
-                                HStack {
-                                    Image("ShieldSymbol")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                    Text(detailHeroViewModel.getHeroBaseArmor())
-                                }
-                                
-                                HStack {
-                                    Image("MovingSpeedSymbol")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                    Text(detailHeroViewModel.getHeroMovingSpeed())
-                                }
-                            }
-                            
-                            VStack {
-                                HStack {
-                                    Image(systemName: "heart.fill")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                        .foregroundColor(.green)
-                                    Text(detailHeroViewModel.getHeroHealth())
-                                        .padding(2)
-                                        .background {
-                                            Color.green
-                                        }
-                                }
-                                
-                                HStack {
-                                    Image(systemName: "heart.fill")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                        .foregroundColor(Color("BlueColaColor"))
-                                    Text(detailHeroViewModel.getHeroHealth())
-                                        .padding(2)
-                                        .background {
-                                            Color("BlueColaColor")
-                                        }
-                                }
-                            }
-                        }
-                    }
-                    .padding(5)
-                    .foregroundColor(.white)
+                    HeroStatsView()
+                        .environmentObject(detailHeroViewModel)
                 }
                 .frame(width: 380, height: 220)
             }
@@ -137,5 +63,87 @@ struct DetailHeroView: View {
 struct DetailHeroView_Previews: PreviewProvider {
     static var previews: some View {
         DetailHeroView(dotaName: "Axe")
+    }
+}
+
+struct HeroStatsView: View {
+    @EnvironmentObject var detailHeroViewModel: DetailHeroViewModel
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Stats")
+            HStack(spacing: 20) {
+                VStack(alignment: .leading) {
+                    Text("Attributes")
+                    HStack {
+                        Image("Strength")
+                        Text(detailHeroViewModel.getHeroStrength())
+                    }
+                    
+                    HStack {
+                        Image("Agility")
+                        Text(detailHeroViewModel.getHeroAgility())
+                    }
+                    
+                    HStack {
+                        Image("Intelligence")
+                        Text(detailHeroViewModel.getHeroIntelligence())
+                    }
+                }
+                Color(.gray)
+                    .frame(width: 1, height: 150)
+                
+                VStack(alignment: .leading) {
+                    Text("Ability")
+                    HStack {
+                        Image("AttackSymbol")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                        Text(detailHeroViewModel.getHeroBaseAttack())
+                    }
+                    
+                    HStack {
+                        Image("ShieldSymbol")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                        Text(detailHeroViewModel.getHeroBaseArmor())
+                    }
+                    
+                    HStack {
+                        Image("MovingSpeedSymbol")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                        Text(detailHeroViewModel.getHeroMovingSpeed())
+                    }
+                }
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "heart.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(.green)
+                        Text(detailHeroViewModel.getHeroHealth())
+                            .padding(2)
+                            .background {
+                                Color.green
+                            }
+                    }
+                    
+                    HStack {
+                        Image(systemName: "heart.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color("BlueColaColor"))
+                        Text(detailHeroViewModel.getHeroHealth())
+                            .padding(2)
+                            .background {
+                                Color("BlueColaColor")
+                            }
+                    }
+                }
+            }
+        }
+        .padding(5)
+        .foregroundColor(.white)
     }
 }
