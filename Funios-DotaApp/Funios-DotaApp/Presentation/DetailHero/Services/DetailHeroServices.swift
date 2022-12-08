551b7bb0623b25244d4a6986d78dac59faea4116
adedwi1808 <adedwip1808@gmail.com>
2022-12-08 18:58:22 +0700

    ADD: Menambahkan DetailHeroServices

---
+++ Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/Services/DetailHeroServices.swift
@@ -0,0 +1,25 @@
+//
+//  DetailHeroServices.swift
+//  Funios-DotaApp
+//
+//  Created by Ade Dwi Prayitno on 08/12/22.
+//
+
+import Foundation
+
+protocol DotaHeroStatsServices: AnyObject {
+    var networker: NetworkerProtocol {get}
+    func getHeroStats(endPoint: NetworkFactory) async throws -> DotaHeroesStatsModel
+}
+
+final class DotaHeroServices: DotaHeroStatsServices {
+    var networker: NetworkerProtocol
+    
+    init(networker: NetworkerProtocol = Networker()) {
+        self.networker = networker
+    }
+    
+    func getHeroStats(endPoint: NetworkFactory) async throws -> DotaHeroesStatsModel {
+        return try await networker.taskAsync(type: DotaHeroesStatsModel.self, endPoint: endPoint, isMultipart: false)
+    }
+}
