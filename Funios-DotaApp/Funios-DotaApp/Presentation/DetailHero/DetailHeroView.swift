f0a4adfa11905ed7e803ebc40c0946db3d18064f
adedwi1808 <adedwip1808@gmail.com>
2022-12-08 10:01:12 +0700

    ADD: DetailHeroView

---
+++ Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/DetailHeroView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailHeroView.swift
+//  Funios-DotaApp
+//
+//  Created by Ade Dwi Prayitno on 08/12/22.
+//
+
+import SwiftUI
+
+struct DetailHeroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailHeroView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailHeroView()
+    }
+}
