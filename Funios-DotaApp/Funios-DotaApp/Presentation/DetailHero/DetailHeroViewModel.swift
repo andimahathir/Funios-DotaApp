b3f3eef2b2f657e70f354a615f29ded57d1dcf1b
adedwi1808 <adedwip1808@gmail.com>
2022-12-08 12:43:21 +0700

    ADD: DetailHeroViewModel

---
+++ Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/DetailHeroViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailHeroViewModel.swift
+//  Funios-DotaApp
+//
+//  Created by Ade Dwi Prayitno on 08/12/22.
+//
+
+import Foundation
+
+class DetailHeroViewModel: ObservableObject {
+    
+}
