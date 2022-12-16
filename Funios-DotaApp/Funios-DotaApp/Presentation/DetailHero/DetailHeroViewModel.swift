65f2cb969abd7dca17a720e6288893b43218d3dd
adedwi1808 <adedwip1808@gmail.com>
2022-12-16 23:28:14 +0700

    FIX: memperbaiki logic untuk pengambilan data dari array dotaHero

--- Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/DetailHeroViewModel.swift
+++ Funios-DotaApp/Funios-DotaApp/Presentation/DetailHero/DetailHeroViewModel.swift
@@ -8,35 +8,39 @@
 import Foundation
 
 class DetailHeroViewModel: ObservableObject {
-    @Published var dotaHero: DotaHeroesStatsModel = []
+    @Published var dotaHero: DotaHeroesStatsModel = DotaHeroesStatsModel()
     
     private var detailHeroStatsServices: DetailHeroStatsServicesProtocol
     
     init(detailHeroStatsServices: DetailHeroStatsServicesProtocol = DetailHeroStatsServices()) {
         self.detailHeroStatsServices = detailHeroStatsServices
-        dotaHero.append(DotaHeroStatsDummy)
     }
     
     func getHeroStats(heroName: String) async {
         do {
             let data = try await detailHeroStatsServices.getHeroStats(endPoint: .getHeroStats)
-            
-            self.dotaHero = data.filter {$0.localizedName == heroName}
+            filterDotaHero(data, heroName)
         } catch {
             print("err while getHeroStats")
         }
     }
     
+    func filterDotaHero(_ data: DotaHeroesStatsModel, _ heroName: String) {
+        DispatchQueue.main.async {
+            self.dotaHero = data.filter {$0.localizedName == heroName}
+        }
+    }
+    
     func getHeroImage() -> String {
-        return "https://api.opendota.com\(dotaHero[0].img)"
+        return "https://api.opendota.com\(dotaHero.first?.img ?? "")"
     }
     
     func getHeroName() -> String {
-        return dotaHero[0].localizedName
+        return dotaHero.first?.localizedName ?? ""
     }
     
     func getHeroPrimaryAttribute() -> String {
-        switch dotaHero[0].primaryAttr {
+        switch dotaHero.first?.primaryAttr {
         case "agi":
             return "Agility"
         case "str":
@@ -49,38 +53,38 @@ class DetailHeroViewModel: ObservableObject {
     }
     
     func getHeroRoles() -> [String] {
-        return dotaHero[0].roles
+        return dotaHero.first?.roles ?? [""]
     }
     
     func getHeroStrength() -> String {
-        return "\(dotaHero[0].baseStr) + \(dotaHero[0].strGain)"
+        return "\(dotaHero.first?.baseStr ?? 0) + \(dotaHero.first?.strGain ?? 0)"
     }
     
     func getHeroAgility() -> String {
-        return "\(dotaHero[0].baseAgi) + \(dotaHero[0].agiGain)"
+        return "\(dotaHero.first?.baseAgi ?? 0) + \(dotaHero.first?.agiGain ?? 0)"
     }
     
     func getHeroIntelligence() -> String {
-        return "\(dotaHero[0].baseInt) + \(dotaHero[0].intGain)"
+        return "\(dotaHero.first?.baseInt ?? 0) + \(dotaHero.first?.intGain ?? 0)"
     }
     
     func getHeroBaseAttack() ->  String {
-        return "\(dotaHero[0].baseAttackMin) - \(dotaHero[0].baseAttackMax)"
+        return "\(dotaHero.first?.baseAttackMin ?? 0) - \(dotaHero.first?.baseAttackMax ?? 0)"
     }
     
     func getHeroBaseArmor() ->  String {
-        return "\(dotaHero[0].baseArmor)"
+        return "\(dotaHero.first?.baseArmor ?? 0)"
     }
     
     func getHeroMovingSpeed() ->  String {
-        return "\(dotaHero[0].baseArmor)"
+        return "\(dotaHero.first?.baseArmor ?? 0)"
     }
     
     func getHeroHealth() -> String {
-        return "\(dotaHero[0].baseHealth)+\(dotaHero[0].baseHealthRegen)"
+        return "\(dotaHero.first?.baseHealth ?? 0)+\(dotaHero.first?.baseHealthRegen ?? 0)"
     }
     
     func getHeroMana() -> String {
-        return "\(dotaHero[0].baseMana)+\(dotaHero[0].baseManaRegen)"
+        return "\(dotaHero.first?.baseMana ?? 0)+\(dotaHero.first?.baseManaRegen ?? 0)"
     }
 }
